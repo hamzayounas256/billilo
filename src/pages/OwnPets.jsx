49587ef7b83cea00e0f12618edfcf24239c2bb58
Hamza Younas <deadbody009@gmail.com>
2024-11-28 13:27:22 +0500

    update new features

--- src/pages/OwnPets.jsx
+++ src/pages/OwnPets.jsx
@@ -8,13 +8,13 @@ import { AnimalContext } from "../context/AnimalContext";
 export default function OwnPets() {
 	const { apiLink } = useContext(AnimalContext);
 	const [products, setProducts] = useState([]);
+	const [productsLossFind, setProductsLossFind] = useState([]);
 
 	const uid = localStorage.getItem("id");
 
 	// Fetch dashboard data
 	const fetchDashboardData = async () => {
 		try {
-			// console.log("Fetching with Range:", priceRange, startDate, endDate); // Debug log
 			const response = await axios.get(apiLink + "/get-pet/", {
 				params: {
 					user_id: uid,
@@ -22,8 +22,25 @@ export default function OwnPets() {
 			});
 
 			if (response.data.success) {
-				console.log("Fetched Products:", response.data.data); // Debug log
 				setProducts(response.data.data);
+			} else {
+				console.error("Error Fetching Products:", response.data.message);
+			}
+		} catch (err) {
+			console.error("API Error:", err.message);
+		}
+	};
+
+	const fetchFindLost = async () => {
+		try {
+			const response = await axios.get(apiLink + "/get_find_or_lost/", {
+				params: {
+					user_id: uid,
+				},
+			});
+
+			if (response.data.success) {
+				setProductsLossFind(response.data.data);
 			} else {
 				console.error("Error Fetching Products:", response.data.message);
 			}
@@ -36,15 +53,36 @@ export default function OwnPets() {
 		fetchDashboardData(); // Trigger fetch on filter changes
 	}, []);
 
+	useEffect(() => {
+		fetchFindLost(); // Trigger fetch on filter changes
+	}, []);
+
 	return (
-		<div className="container mx-auto flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+		<div className="container mx-auto flex flex-col justify-center sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
 			{/* Products Section */}
 			<div className="md:mx-2">
 				<div className="flex justify-center text-base sm:text-2xl mb-4">
-					<Title text1={"OWN"} text2={"PETS"} />
+					<Title text1={"OWN SALE &"} text2={"ADOPT PETS"} />
 				</div>
-				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4 gap-y-6">
+				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6 gap-y-6">
 					{products.map((item) => (
+						<ProductOwn
+							key={item.id}
+							id={item.id}
+							status={item.status}
+							image={item.images}
+							name={item.name}
+							category={item.category}
+							breed={item.breed}
+							location={item.location}
+						/>
+					))}
+				</div>
+				<div className="flex justify-center text-base sm:text-2xl my-5 border-t">
+					<Title text1={"OWN FIND &"} text2={"LOST PETS"} />
+				</div>
+				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6 gap-y-6">
+					{productsLossFind.map((item) => (
 						<ProductOwn
 							key={item.id}
 							id={item.id}
