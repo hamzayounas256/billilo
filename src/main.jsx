e38554616d084996c8d979830fb7e65e29c7842d
Hamza Younas <deadbody009@gmail.com>
2024-11-28 23:57:47 +0500

    remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import AnimalContextProvider from "./context/AnimalContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<BrowserRouter basename="/billilo">
+	<BrowserRouter>
 		<AnimalContextProvider>
 			<App />
 		</AnimalContextProvider>
